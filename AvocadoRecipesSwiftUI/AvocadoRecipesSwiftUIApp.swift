dc64c9b17d3c90cab9a3acab7c728e2fbc9621b6
Khavi18 <khavishini18@gmail.com>
2024-07-06 15:35:54 +0800

    Initial Commit

---
+++ AvocadoRecipesSwiftUI/AvocadoRecipesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoRecipesSwiftUIApp.swift
+//  AvocadoRecipesSwiftUI
+//
+//  Created by Khavishini on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoRecipesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
